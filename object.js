aeaa72f48c319150c63bbaa61672ea9a2bda433a
SujitBhusane322 <sujitbhusane322@gmail.com>
2024-07-17 18:10:29 +0530

    js object

---
+++ object.js
@@ -0,0 +1,18 @@
+let student=[{
+    name:"sujit",
+    roll_no:121,
+    role:"monitor",
+
+    },
+    {
+        name:"jay",
+        roll_no:122,
+        role:"monitor",
+    }];
+
+for(i=0;i<student.length;i++){
+    stud=student[i];
+    console.log(stud.roll_no);
+}
+
+
